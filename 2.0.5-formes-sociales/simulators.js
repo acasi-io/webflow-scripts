72b8c3e2d72159f2e005c5ae01c07c23e1377bf4
Lauryne Tranchant <laurynetranchant@MacBook-Pro-de-Jonathan.local>
2023-03-31 16:45:06 +0200

    add code webflow forme sociale resultat

--- 2.0.5-formes-sociales/simulators.js
+++ 2.0.5-formes-sociales/simulators.js
@@ -464,16 +464,16 @@ if (result === 'SARL ou SAS' || result === 'EURL ou SASU') {
         showExplanation('SASU', secondExplanation); 
     }
 } else {
-  const resultText = document.getElementById('simulator-result-text');
-  resultText.innerHTML = `${result}`; 
-  const explanationContainer = document.querySelector('.simulator-result-explanation-container');
-  showExplanation(result, explanationContainer); 
+    const resultText = document.getElementById('simulator-result-text');
+    resultText.innerHTML = `${result}`; 
+    const explanationContainer = document.querySelector('.simulator-result-explanation-container');
+    showExplanation(result, explanationContainer); 
 }
 
 
 function showExplanation(result, explanationContainer) {
 	const explanationText = document.getElementById(`simulator-${result}-explanation`);
-  explanationContainer.classList.remove('simulator-hidden'); 
+    explanationContainer.classList.remove('simulator-hidden'); 
 	explanationText.classList.remove('simulator-hidden');
 	explanationContainer.append(explanationText);
 }
