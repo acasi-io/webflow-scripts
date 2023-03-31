ee426d72e2d1297b16286e862da298d3fe976f7f
Lauryne Tranchant <laurynetranchant@MacBook-Pro-de-Jonathan.local>
2023-03-31 16:40:56 +0200

    add code webflow forme sociale resultat

--- 2.0.1-formes-sociales/simulators.js
+++ 2.0.1-formes-sociales/simulators.js
@@ -434,3 +434,46 @@ function highlightCards(choice, answer) {
         answer.style.boxShadow = "0px 0px 10px #132966"; 
     }
 }
+
+
+
+/* 
+
+CODE WEBFLOW FORME SOCIALE RESULTAT 
+
+const result = localStorage.getItem('result');  
+const firstAnswer = document.getElementById('first-answer'); 
+const secondAnswer = document.getElementById('second-answer'); 
+
+if (result === 'SARL ou SAS' || result === 'EURL ou SASU') {
+	document.getElementById('one-answer').classList.add('simulator-hidden'); 
+    document.getElementById('two-answers').classList.remove('simulator-hidden');
+    const firstExplanation = document.getElementById('first-explanation'); 
+    const secondExplanation = document.getElementById('second-explanation'); 
+    if (result === 'SARL ou SAS') {
+  	    firstAnswer.innerHTML = 'SARL'; 
+        showExplanation('SARL', firstExplanation); 
+        secondAnswer.innerHTML = 'SAS'; 
+        showExplanation('SAS', secondExplanation); 
+    } else if (result === 'EURL ou SASU') {
+  	    firstAnswer.innerHTML = 'EURL'; 
+        showExplanation('EURL', firstExplanation); 
+        secondAnswer.innerHTML = 'SASU'; 
+        showExplanation('SASU', secondExplanation); 
+    }
+} else {
+    const resultText = document.getElementById('simulator-result-text');
+    resultText.innerHTML = `${result}`; 
+    const explanationContainer = document.querySelector('.simulator-result-explanation-container');
+    showExplanation(result, explanationContainer); 
+}
+
+
+function showExplanation(result, explanationContainer) {
+	const explanationText = document.getElementById(`simulator-${result}-explanation`);
+    explanationContainer.classList.remove('simulator-hidden'); 
+	explanationText.classList.remove('simulator-hidden');
+	explanationContainer.append(explanationText);
+}
+
+*/
