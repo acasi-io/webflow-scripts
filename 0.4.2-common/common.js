b016641b76f33b3eae44ac53a1562f9b9193594c
Lauryne Tranchant <laurynetranchant@MacBook-Pro-de-Jonathan.local>
2023-04-19 11:57:56 +0200

    clean code test

--- 0.4.2-common/common.js
+++ 0.4.2-common/common.js
@@ -67,122 +67,6 @@ previousBtn.addEventListener('click', () => {
 }); 
 
 
-/*function firstQuestion() {
-    const firstQuestionData = questionsData.find(question => question.id === questionIndex);
-    setItemStorage('indexCurrentQuestion', firstQuestionData.id); 
-    fillQuestionTitleTheme(firstQuestionData);  
-       
-    generateQuestion(firstQuestionData); 
-}
-function generateQuestion(currentQuestion) {
-    const answerBlock = document.getElementById('answer-block').firstChild; 
-    simulatorBlock.innerHTML = '';
-    //appendHubspotProperty(currentQuestion);
-    currentQuestion.choices.forEach((choice, index) => {
-        let answer; 
-        showQuestion(answerBlock, currentQuestion, choice, index, answer)
-    }); 
-}
-function showQuestion(answerBlock, currentQuestion, choice, index, answer) {
-    const cloneAnswerBlock = answerBlock.cloneNode(true); 
-    simulatorBlock.appendChild(cloneAnswerBlock); 
-    answer = simulatorBlock.children[index];
-    const { id, value, image, hubspotValue } = choice;
-    const input = answer.querySelector('.simulator-radio'); 
-    input.setAttribute('id', id); 
-    input.setAttribute('value', id);
-    input.setAttribute('data-hubspot-value', hubspotValue);
-    const label = answer.querySelector('.simulator-answer'); 
-    label.textContent = value; 
-    label.setAttribute('for', id); 
- 
-    const emoji = answer.querySelector('.simulator-emoji');
-    if (image) { 
-        emoji.textContent = image; 
-    } else {
-        emoji.remove(); 
-    }
-    highlightCards(currentQuestion, answer);
-    computeQuestion(currentQuestion, input, answer); 
-}
-function computeQuestion(currentQuestion, input, answer) {
-    answer.addEventListener('click', () => { 
-        setItemStorage('indexCurrentChoice', input.id);   
-    });
-    input.addEventListener('click', (e) => {
-        [...document.querySelectorAll('.simulator-answer-btn')].forEach(element => {
-            element.classList.remove('simulator-checked'); 
-        });
-        e.currentTarget.parentNode.classList.add('simulator-checked');
-        const hubspotPropertyBlock = hubspotPropertiesBlock.querySelector(`[data-hubspot-property='${currentQuestion.property}']`)
-        hubspotPropertyBlock.querySelector('input').setAttribute("value", e.currentTarget.dataset.hubspotValue);
-        updateLocalStorage(currentQuestion);
-        const currentChoice = parseInt(localStorage.getItem('indexCurrentChoice')); 
-        nextQuestion(); 
-    });
-}
-function appendHubspotProperty(currentQuestion) {
-    const property = currentQuestion.property;
-    if (property) {
-        hubspotPropertiesBlock.insertAdjacentHTML('beforeend', `<div data-hubspot-property="${property}" style='visibility: hidden; height: 0'><label>${property}</label><input type='text'/></div>`)
-    }
-}
-function updateLocalStorage(currentQuestion) {
-    const currentChoiceIndex = parseInt(localStorage.getItem('indexCurrentChoice')); 
-    const currentChoiceData = currentQuestion.choices.find(data => data.id === currentChoiceIndex)
-    setItemStorage('indexNextQuestion', currentChoiceData.nextQuestion);
-}
-function updateResultArray(currentChoice, currentQuestion) {
-    if (currentChoice.result === true) {
-        const newResult = {}; 
-        newResult.question = `${currentQuestion.question}`;
-        newResult.result = `${currentChoice.resultValue}`;
-        resultArray.push(newResult); 
-        setItemStorage('result', currentChoice.resultValue); 
-    }
-}
-function forShowForm(formTemplate) {
-    simulatorOptions.innerHTML = ''; 
-    questionTitle.innerHTML = 'Entrez vos coordonnées pour afficher le résultat de la simulation';
-    questionTheme.innerHTML = 'Résultat'; 
-    addHiddenClass(nextBtn); 
-    addHiddenClass(previousBtn); 
-    removeHiddenClass(formTemplate);  
-    addHiddenClass(document.getElementById('simulator-information'));
-}
-let currentChoiceData; 
-let currentQuestionData; 
-function findQuestionForStoreResult(questionsData) {
-    const indexCurrentChoice = parseInt(localStorage.getItem('indexCurrentChoice')); 
-    const indexCurrentQuestion = parseInt(localStorage.getItem('indexCurrentQuestion')); 
-    currentQuestionData = questionsData.find(question => question.id === indexCurrentQuestion); 
-    currentChoiceData = currentQuestionData.choices.find(choice => choice.id === indexCurrentChoice); 
-    updateResultArray(currentChoiceData, currentQuestionData);
-    //updatePreviousQuestionArray(currentQuestionData, currentChoiceData); 
-}
-function getNextQuestion(questionsData) {
-    const indexCurrentQuestion = parseInt(localStorage.getItem('indexNextQuestion')); 
-    const currentQuestionData = questionsData.find(question => question.id === indexCurrentQuestion); 
-    setItemStorage('indexCurrentQuestion', currentQuestionData.id);
-    fillQuestionTitleTheme(currentQuestionData); 
-    generateQuestion(currentQuestionData); 
-}
-function highlightCards(currentQuestion, answer) {
-    if (currentQuestion.highlight === true) {
-        answer.style.boxShadow = "0px 0px 10px #132966"; 
-    }
-}*/ 
-
-
-/*function updatePreviousQuestionArray(currentQuestion, currentChoice) {
-    const newValue = new Object();  
-    newValue.question = `${currentQuestion.questionTree}`; 
-    newValue.value = `${currentChoice.value}`; 
-    setItemStorage('indexPreviousQuestion', currentQuestion.questionTree); 
-    previousQuestionArray.push(newValue); 
-}*/
-
-
 function firstQuestion() {
     const firstQuestionData = questionsData.find(question => question.id === questionIndex);
     setItemStorage('indexCurrentQuestion', firstQuestionData.id); 
@@ -240,12 +124,12 @@ function showQuestion(currentQuestion) {
 }
 
 
-function appendHubspotProperty(currentQuestion) {
+/*function appendHubspotProperty(currentQuestion) {
     const property = currentQuestion.property;
     if (property) {
         hubspotPropertiesBlock.insertAdjacentHTML('beforeend', `<div data-hubspot-property="${property}" style='visibility: hidden; height: 0'><label>${property}</label><input type='text'/></div>`)
     }
-}
+}*/
 
 
 function updateLocalStorage(currentQuestion) {
@@ -257,7 +141,7 @@ function updateLocalStorage(currentQuestion) {
 
 function updateResultArray(currentChoice, currentQuestion) {
     if (currentChoice.result === true) {
-        const newResult = new Object(); 
+        const newResult = {}; 
         newResult.question = `${currentQuestion.question}`;
         newResult.result = `${currentChoice.resultValue}`;
         resultArray.push(newResult); 
@@ -328,7 +212,7 @@ function storeResult(questionsData) {
 
 
 function updatePreviousQuestionArray(currentQuestion, currentChoice) {
-    const newValue = new Object();  
+    const newValue = {};  
     newValue.question = `${currentQuestion.questionTree}`; 
     newValue.value = `${currentChoice.value}`; 
     setItemStorage('previousQuestion', currentQuestion.questionTree); 
