9afb783dfe85ecb256d33b610948f130d450c948
Lauryne Tranchant <laurynetranchant@MacBook-Pro-de-Jonathan-2.local>
2024-01-19 15:11:26 +0100

    add all the functions for sasuResult

--- simulators/0.0.4-remuneration-independants/script.js
+++ simulators/0.0.4-remuneration-independants/script.js
@@ -1,5 +1,5 @@
-import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/0.0.3-remuneration-independants/node_modules/publicodes/dist/index.js';
-import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/0.0.3-remuneration-independants/node_modules/modele-social/dist/index.js';
+import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/0.0.4-remuneration-independants/node_modules/publicodes/dist/index.js';
+import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/0.0.4-remuneration-independants/node_modules/modele-social/dist/index.js';
 
 const engine = new Engine(rules);
 
@@ -13,6 +13,7 @@ document.getElementById('calcul-btn').addEventListener('click', () => {
     const turnoverMinusCost = turnover - cost;
 
     eurlResult(turnoverMinusCost, situation, cost, numberOfChild, householdIncome);
+    sasuResult(turnoverMinusCost, situation, numberOfChild, householdIncome);
 });
 
 
@@ -38,7 +39,7 @@ function retirementText(gainTrimesterTag, pensionSchemeTag, retirementPointsTag)
     document.getElementById(gainTrimesterTag).textContent = gainTrimester.nodeValue;
 
     const pensionScheme = engine.evaluate("protection sociale . retraite . base");
-    document.getElementById(pensionSchemeTag).textContent = pensionScheme.nodeValue * 12;
+    document.getElementById(pensionSchemeTag).textContent = `${pensionScheme.nodeValue * 12} €/an`;
 
     const retirementPoints = engine.evaluate("protection sociale . retraite . complémentaire . RCI . points acquis");
     document.getElementById(retirementPointsTag).textContent = retirementPoints.nodeValue;
@@ -64,7 +65,6 @@ function eurlResult(turnoverMinusCost, situation, cost, numberOfChild, household
         eurlRemuneration('.ir-eurl-before', '.ir-eurl-after');
     }
 }
-
 
 function eurlSituation(turnoverMinusCost, situation, cost, numberOfChild, householdIncome, tax, singleParent) {
     engine.setSituation({
@@ -101,4 +101,61 @@ function eurlContributions() {
 
 function eurlRetirement() {
     retirementText('eurl-gain-trimester', 'eurl-pension-scheme', 'eurl-retirement-points');
+}
+
+
+/* SASU */
+
+function sasuResult(turnoverMinusCost, situation, numberOfChild, householdIncome) {
+    sasuSituation(turnoverMinusCost, situation, numberOfChild, householdIncome, 'non');
+
+    sasuRemuneration();
+    sasuContributions();
+
+    if(document.getElementById('checkbox-single-parent').checked) {
+        sasuSituation(turnoverMinusCost, situation, numberOfChild, householdIncome, 'oui');
+        sasuRemuneration('.is-eurl-before', '.is-eurl-after');
+    }
+}
+
+function sasuSituation(turnoverMinusCost, situation, numberOfChild, householdIncome, singleParent) {
+    engine.setSituation({
+        "dirigeant . rémunération . totale": parseInt(turnoverMinusCost),
+        "impôt . foyer fiscal . situation de famille": `'${situation}'`,
+        "impôt . foyer fiscal . enfants à charge": parseInt(numberOfChild),
+        "impôt . foyer fiscal . revenu imposable . autres revenus imposables": parseFloat(householdIncome),
+        "impôt . foyer fiscal . parent isolé": `${singleParent}`,
+        "salarié . régimes spécifiques . DFS": "non",
+        "impôt . méthode de calcul": "'barème standard'",
+        "entreprise . catégorie juridique": "'SAS'",
+    });
+}
+
+function sasuRemuneration() {
+    fillSameClassTexts("salarié . rémunération . net . à payer avant impôt", '.sasu-before');
+    fillSameClassTexts("salarié . rémunération . net . payé après impôt", '.sasu-after');
+}
+
+function sasuContributions() {
+    fillText("dirigeant . assimilé salarié . cotisations", '#sasu-contributions-total');
+
+    /* EMPLOYER */
+    fillText("salarié . cotisations . maladie . employeur", '#sasu-disease');
+    fillText("salarié . cotisations . CSA", '#sasu-solidarity-autonomy');
+    fillText("salarié . cotisations . ATMP", '#sasu-work-accident');
+    fillText("salarié . cotisations . vieillesse . employeur", '#sasu-employer-old-age');
+    fillText("salarié . cotisations . retraite complémentaire . employeur", '#sasu-employer-additional-retirement');
+    fillText("salarié . cotisations . CEG . employeur", '#sasu-employer-general-balance');
+    fillText("salarié . cotisations . allocations familiales", '#sasu-family-allowance');
+    fillText("salarié . cotisations . FNAL", '#sasu-fnal');
+    fillText("salarié . cotisations . formation professionnelle", '#sasu-formation');
+    fillText("salarié . cotisations . taxe d'apprentissage", '#sasu-learning-tax');
+    fillText("salarié . cotisations . prévoyances . employeur", '#sasu-additional-planning');
+
+    /* EMPLOYEE */
+    fillText("salarié . cotisations . vieillesse . salarié", '#sasu-employee-old-age');
+    fillText("salarié . cotisations . retraite complémentaire . salarié", '#sasu-employee-additional-retirement');
+    fillText("salarié . cotisations . CEG . salarié", '#sasu-employee-general-balance');
+    fillText("salarié . cotisations . CSG-CRDS", '#sasu-csg');
+    fillText("salarié . cotisations . prévoyances . salarié", '#sasu-employee-additional-planning');
 }
