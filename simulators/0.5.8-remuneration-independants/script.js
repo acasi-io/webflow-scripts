7ddbdb8df74a2bbccc320f54e0e4b97573a7f3d8
Lauryne Tranchant <laurynetranchant@MacBook-Pro-de-Jonathan-2.local>
2024-01-26 17:01:34 +0100

    test dividendes nets

--- simulators/0.5.8-remuneration-independants/script.js
+++ simulators/0.5.8-remuneration-independants/script.js
@@ -1,5 +1,5 @@
-import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/0.5.7-remuneration-independants/node_modules/publicodes/dist/index.js';
-import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/0.5.7-remuneration-independants/node_modules/modele-social/dist/index.js';
+import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/0.5.8-remuneration-independants/node_modules/publicodes/dist/index.js';
+import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/0.5.8-remuneration-independants/node_modules/modele-social/dist/index.js';
 
 const engine = new Engine(rules);
 
@@ -191,6 +191,8 @@ function calculWageAndDividends(turnoverMinusCost, numberOfChild, householdIncom
 
     console.log(maxWageIfAllWage);
     console.log(maxDividendsIfAllDividends);
+
+    console.log(numberOfChild);
 
     sasuCalculDividendsNets(maxDividendsIfAllDividends);
 
