7dbd47b70865064300d6855e751bc67cc356500b
Lauryne Tranchant <laurynetranchant@mbpdejonathan2.home>
2024-01-31 16:45:25 +0100

    test myArray

--- simulators/1.0.8-remuneration-independants/script.js
+++ simulators/1.0.8-remuneration-independants/script.js
@@ -1,5 +1,5 @@
-import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/1.0.7-remuneration-independants/node_modules/publicodes/dist/index.js';
-import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/1.0.7-remuneration-independants/node_modules/modele-social/dist/index.js';
+import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/1.0.8-remuneration-independants/node_modules/publicodes/dist/index.js';
+import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/1.0.8-remuneration-independants/node_modules/modele-social/dist/index.js';
 
 const engine = new Engine(rules);
 
@@ -254,8 +254,6 @@ function calculDividends(turnoverMinusCost, numberOfChild, householdIncome, situ
 }
 
 function sasuCalculDividendsNets(dividends, situation, numberOfChild, householdIncome, singleParent, afterTax, percentage, myArray) {
-    myArray = JSON.parse(localStorage.getItem('myArray'));
-
     /* Dividendes Barème Progressif */
     engine.setSituation({
         "bénéficiaire . dividendes . bruts": parseInt(dividends),
