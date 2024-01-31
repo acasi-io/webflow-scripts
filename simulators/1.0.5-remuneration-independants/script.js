d89055919eb0f517351d6070caa42c5a5b0ca6b0
Lauryne Tranchant <laurynetranchant@mbpdejonathan2.home>
2024-01-31 15:59:24 +0100

    test boucle for

--- simulators/1.0.5-remuneration-independants/script.js
+++ simulators/1.0.5-remuneration-independants/script.js
@@ -1,5 +1,5 @@
-import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/1.0.4-remuneration-independants/node_modules/publicodes/dist/index.js';
-import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/1.0.4-remuneration-independants/node_modules/modele-social/dist/index.js';
+import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/1.0.5-remuneration-independants/node_modules/publicodes/dist/index.js';
+import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/1.0.5-remuneration-independants/node_modules/modele-social/dist/index.js';
 
 const engine = new Engine(rules);
 
@@ -191,7 +191,7 @@ function sasuCalculAll(turnoverMinusCost, situation, numberOfChild, householdInc
         maxDividends = Math.round(totalForIs - ((42500 * 0.15) + ((totalForIs - 42500) * 0.25)));
     }
 
-    sasuCalculDividendsNets(maxDividends, situation, numberOfChild, householdIncome, 'non', afterTaxAmount);
+    sasuCalculDividendsNets(maxDividends, situation, numberOfChild, householdIncome, 'non', afterTax);
 }
 
 
