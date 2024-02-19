fc2a13be1972ad7686e48c9a5b3f174e9ecef0c6
Lauryne Tranchant <laurynetranchant@mbpdejonathan2.home>
2024-02-19 17:01:17 +0100

    add eurlDividends for testing calcul of dividends

--- simulators/2.0.8-remuneration-independants/script.js
+++ simulators/2.0.8-remuneration-independants/script.js
@@ -1,5 +1,5 @@
-import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/2.0.7-remuneration-independants/node_modules/publicodes/dist/index.js';
-import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/2.0.7-remuneration-independants/node_modules/modele-social/dist/index.js';
+import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/2.0.8-remuneration-independants/node_modules/publicodes/dist/index.js';
+import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/2.0.8-remuneration-independants/node_modules/modele-social/dist/index.js';
 
 const engine = new Engine(rules);
 
@@ -633,7 +633,7 @@ function eurlDividends(turnoverMinusCost, situation, numberOfChild, householdInc
     // si on prend 10% du CA en rémunération
     let eurlArray = [];
     let percentage = 0;
-    let wage = turnoverMinusCost * percentage;
+    let wage = turnoverMinusCost * (percentage / 100);
     console.log(wage);
     let wageAfter = wage + (turnoverMinusCost * (5 / 100));
 
