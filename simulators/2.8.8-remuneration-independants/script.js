9ddc8324fa834f394e135fce825ae10d355b0c79
Lauryne Tranchant <laurynetranchant@mbpdejonathan2.home>
2024-06-19 15:25:44 +0200

    add localStorage to verify the results

--- simulators/2.8.8-remuneration-independants/script.js
+++ simulators/2.8.8-remuneration-independants/script.js
@@ -1,5 +1,5 @@
-import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/2.8.7-remuneration-independants/node_modules/publicodes/dist/index.js';
-import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/2.8.7-remuneration-independants/node_modules/modele-social/dist/index.js';
+import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/2.8.8-remuneration-independants/node_modules/publicodes/dist/index.js';
+import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/2.8.8-remuneration-independants/node_modules/modele-social/dist/index.js';
 
 const engine = new Engine(rules);
 
@@ -738,6 +738,7 @@ function comparerRemunerations(maxWage, turnoverMinusCost, singleParent, numberO
     }
 
     console.log('Meilleur résultat:', meilleurResultat);
+    localStorage.setItem('eurlArray', meilleurResultat);
     return meilleurResultat;
 }
 
