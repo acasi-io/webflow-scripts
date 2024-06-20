3ec57b4ee7baf97fdc330cff03223f76e53ed656
Lauryne Tranchant <laurynetranchant@mbpdejonathan2.home>
2024-06-20 16:21:34 +0200

    add console.log to see if functions are well imported

--- simulators/3.1.2-remuneration-independants/eurl.js
+++ simulators/3.1.2-remuneration-independants/eurl.js
@@ -1,5 +1,5 @@
-import Engine from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.1.1-remuneration-independants/node_modules/publicodes/dist/index.js';
-import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.1.1-remuneration-independants/node_modules/modele-social/dist/index.js';
+import Engine from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.1.2-remuneration-independants/node_modules/publicodes/dist/index.js';
+import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.1.2-remuneration-independants/node_modules/modele-social/dist/index.js';
 
 import { retirementText, fillText } from './script.js';
 
