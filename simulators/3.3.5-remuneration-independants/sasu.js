a6cd2d5e87367514a69c75124bca2a504e30307a
lauryne <lauryne.tranchant@acasi.io>
2024-08-07 12:11:51 +0200

    add sasuPumaTax function

--- simulators/3.3.5-remuneration-independants/sasu.js
+++ simulators/3.3.5-remuneration-independants/sasu.js
@@ -1,5 +1,5 @@
-import Engine from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.3.4-remuneration-independants/node_modules/publicodes/dist/index.js';
-import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.3.4-remuneration-independants/node_modules/modele-social/dist/index.js';
+import Engine from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.3.5-remuneration-independants/node_modules/publicodes/dist/index.js';
+import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.3.5-remuneration-independants/node_modules/modele-social/dist/index.js';
 
 const engine = new Engine(rules);
 
@@ -157,11 +157,23 @@ function sasuResult(turnoverMinusCost, situation, numberOfChild, householdIncome
     sasuContributions();
     sasuRetirement();
 
+    // calculPumaTax(maxDividends, pumaTaxAmount);
+
+    let PASS = 46368;
+    let halfPass = 0.5 * PASS;
+    let fifthPass = 0.2 * PASS;
+
+    sasuAfterTax = parseInt(localStorage.getItem('sasuAfterTax'));
+
     let pumaTaxAmount;
 
-    calculPumaTax(maxDividends, pumaTaxAmount);
+    if (sasuAfterTax > fifthPass) {
+        pumaTaxAmount = 0;
+    } else {
+        pumaTaxAmount = (0.065 * (maxDividends - halfPass) * (1 - (sasuAfterTax / fifthPass)));
+    }
 
-    pumaTaxAmount = calculPumaTax(maxDividends);
+    // pumaTaxAmount = calculPumaTax(maxDividends);
     document.getElementById('sasu-puma').textContent = pumaTaxAmount.toLocaleString('fr-FR') + '€';
 
     let grossDividends = maxDividends - pumaTaxAmount;
--- simulators/3.3.5-remuneration-independants/script.js
+++ simulators/3.3.5-remuneration-independants/script.js
@@ -1,5 +1,5 @@
-import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.3.4-remuneration-independants/node_modules/publicodes/dist/index.js';
-import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.3.4-remuneration-independants/node_modules/modele-social/dist/index.js';
+import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.3.5-remuneration-independants/node_modules/publicodes/dist/index.js';
+import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.3.5-remuneration-independants/node_modules/modele-social/dist/index.js';
 
 import { calculEurl } from './eurl.js';
 import { microConditions, microResult, fillTextForMicro, microCalculRetraite } from './micro.js';
