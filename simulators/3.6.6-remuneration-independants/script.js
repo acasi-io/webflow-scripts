3c0cf059d47cdf6aa8eb52dcfc9855362866f8e2
lauryne <lauryne.tranchant@acasi.io>
2024-08-28 15:52:13 +0200

    add conditions for bestchoice text

--- simulators/3.6.6-remuneration-independants/script.js
+++ simulators/3.6.6-remuneration-independants/script.js
@@ -1,5 +1,5 @@
-import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.6.5-remuneration-independants/node_modules/publicodes/dist/index.js';
-import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.6.5-remuneration-independants/node_modules/modele-social/dist/index.js';
+import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.6.6-remuneration-independants/node_modules/publicodes/dist/index.js';
+import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.6.6-remuneration-independants/node_modules/modele-social/dist/index.js';
 
 import { calculEurl } from './eurl.js';
 import { microConditions, microResult, fillTextForMicro, microCalculRetraite } from './micro.js';
@@ -38,6 +38,8 @@ window.addEventListener('load', () => {
     numberOfChild.addEventListener('change', (input) => {
         numberOfChildValue = parseInt(input.target.value);
     });
+
+    situationValue = 'célibataire';
 
     if (situationValue === "célibataire" && numberOfChildValue > 0) {
         singleParentSentence.classList.remove('is-hidden');
