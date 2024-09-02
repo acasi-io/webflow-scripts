299e4bd878cfa1966b8963381d6da94e0b21d9e2
lauryne <lauryne.tranchant@acasi.io>
2024-09-02 15:44:36 +0200

    change text best choice

--- simulators/3.7.2-remuneration-independants/script.js
+++ simulators/3.7.2-remuneration-independants/script.js
@@ -1,5 +1,5 @@
-import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.7.1-remuneration-independants/node_modules/publicodes/dist/index.js';
-import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.7.1-remuneration-independants/node_modules/modele-social/dist/index.js';
+import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.7.2-remuneration-independants/node_modules/publicodes/dist/index.js';
+import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.7.2-remuneration-independants/node_modules/modele-social/dist/index.js';
 
 import { calculEurl } from './eurl.js';
 import { microConditions, microResult, fillTextForMicro, microCalculRetraite } from './micro.js';
@@ -313,10 +313,14 @@ function showBestChoice(socialForm) {
     let resultRecapTitle = document.getElementById('simulator-result-title');
 
     resultRecapTitle.textContent = document.getElementById(`${socialForm}-heading-recap`).textContent;
+    resultRecapTitle.style.display = 'none';
 
     let bestChoiceContainer = document.querySelector('.simulator_best_result_container');
 
     let bestChoiceRecap = document.getElementById(`${socialForm}-container-recap`);
+
+    let gridRecap = document.querySelector(`${socialForm}-container-recap .simulator_recap_numbers_grid`);
+    gridRecap.style.display = 'none';
 
     bestChoiceContainer.appendChild(bestChoiceRecap);
 }
