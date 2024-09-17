cb4ef958580863ebae182d1f3d311036617479af
Lauryne Tranchant <lauryne@macbook-pro-de-lauryne.home>
2024-09-17 17:09:49 +0200

    change best result

--- simulators/3.9.7-remuneration-independants/micro.js
+++ simulators/3.9.7-remuneration-independants/micro.js
@@ -1,5 +1,5 @@
-import Engine from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.9.6-remuneration-independants/node_modules/publicodes/dist/index.js';
-import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.9.6-remuneration-independants/node_modules/modele-social/dist/index.js';
+import Engine from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.9.7-remuneration-independants/node_modules/publicodes/dist/index.js';
+import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/3.9.7-remuneration-independants/node_modules/modele-social/dist/index.js';
 
 const engine = new Engine(rules);
 
@@ -154,4 +154,4 @@ function microCalculRetraite(turnover) {
 }
 
 
-export { microConditions, microResult, fillTextForMicro, microCalculRetraite };
+export { microConditions, microResult, microCalculRetraite };
