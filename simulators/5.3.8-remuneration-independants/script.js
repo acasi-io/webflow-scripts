184d13a019d823aadf71569941846894afcacf07
Lauryne Tranchant <lauryne@mac.home>
2024-10-01 11:57:52 +0200

    eurl doesn't display well

--- simulators/5.3.8-remuneration-independants/script.js
+++ simulators/5.3.8-remuneration-independants/script.js
@@ -1,5 +1,5 @@
-import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/5.3.7-remuneration-independants/node_modules/publicodes/dist/index.js';
-import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/5.3.7-remuneration-independants/node_modules/modele-social/dist/index.js';
+import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/5.3.8-remuneration-independants/node_modules/publicodes/dist/index.js';
+import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/5.3.8-remuneration-independants/node_modules/modele-social/dist/index.js';
 
 import { calculEurl, storageEurlTotal, fillEurlComparison } from './eurl.js';
 import { microResult, microCalculRetraite, storageMicroTotal, fillMicroComparison } from './micro.js';
@@ -69,7 +69,7 @@ calculBtn.addEventListener('click', () => {
         const turnoverMinusCost = turnover - cost;
 
         storageMicroTotal(turnoverMinusCost, situationValue, numberOfChildValue, householdIncome, singleParent);
-        fillMicroComparison(turnoverMinusCost, situationValue, numberOfChildValue, householdIncome, singleParent)
+        // fillMicroComparison(turnoverMinusCost, situationValue, numberOfChildValue, householdIncome, singleParent);
 
         storageEiTotal(turnoverMinusCost, situationValue, numberOfChildValue, householdIncome, singleParent);
         // fillEiComparison(turnoverMinusCost, situationValue, numberOfChildValue, householdIncome, singleParent);
