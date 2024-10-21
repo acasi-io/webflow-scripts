8aa7198543e3633cf1ad15789ce8180141f1f302
Lauryne Tranchant <lauryne@macbook-pro-de-lauryne-1.home>
2024-10-21 21:48:15 +0200

    change order remuneration

--- simulators/5.9.8-remuneration-independants/script.js
+++ simulators/5.9.8-remuneration-independants/script.js
@@ -1,5 +1,5 @@
-import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/5.9.7-remuneration-independants/node_modules/publicodes/dist/index.js';
-import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/5.9.7-remuneration-independants/node_modules/modele-social/dist/index.js';
+import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/5.9.8-remuneration-independants/node_modules/publicodes/dist/index.js';
+import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/5.9.8-remuneration-independants/node_modules/modele-social/dist/index.js';
 
 import { calculEurl, storageEurlTotal, fillEurlComparison } from './eurl.js';
 import { microResult, microCalculRetraite, storageMicroTotal, fillMicroComparison } from './micro.js';
@@ -254,6 +254,11 @@ function fillBestChoiceText(turnover, situationValue, bestSocialForm) {
 
     //orderResults(sasuFinalAmount, eurlFinalAmount, eiFinalAmount, microFinalAmount);
     orderBestRemuneration(sasuFinalAmount, eurlFinalAmount, eiFinalAmount, microFinalAmount);
+
+    document.querySelector('[data-socialform="EURL"').textContent = eurlFinalAmount.toLocaleString('fr-FR') + '€';
+    document.querySelector('[data-socialform="SASU"').textContent = sasuFinalAmount.toLocaleString('fr-FR') + '€';
+    document.querySelector('[data-socialform="EI"').textContent = eiFinalAmount.toLocaleString('fr-FR') + '€';
+    document.querySelector('[data-socialform="MICRO"').textContent = microFinalAmount.toLocaleString('fr-FR') + '€';
 
     const contributionsTotal = document.getElementById('contributions-total');
     document.getElementById('best-contributions').textContent = contributionsTotal.textContent;
