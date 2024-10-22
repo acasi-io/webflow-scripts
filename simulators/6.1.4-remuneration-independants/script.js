233880f058f34c6b8ce2deedaf1a1e18960697ec
Lauryne Tranchant <lauryne@macbook-pro-de-lauryne-1.home>
2024-10-22 11:57:25 +0200

    6.1.4 - show rectangle

--- simulators/6.1.4-remuneration-independants/script.js
+++ simulators/6.1.4-remuneration-independants/script.js
@@ -1,5 +1,5 @@
-import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/6.1.3-remuneration-independants/node_modules/publicodes/dist/index.js';
-import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/6.1.3-remuneration-independants/node_modules/modele-social/dist/index.js';
+import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/6.1.4-remuneration-independants/node_modules/publicodes/dist/index.js';
+import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/6.1.4-remuneration-independants/node_modules/modele-social/dist/index.js';
 
 import { calculEurl, storageEurlTotal, fillEurlComparison } from './eurl.js';
 import { microResult, microCalculRetraite, storageMicroTotal, fillMicroComparison } from './micro.js';
@@ -391,6 +391,10 @@ function updateTextOrder(eurlFinalAmount, sasuFinalAmount, eiFinalAmount, microF
     if (rectangle) {
         rectangle.style.order = index;  // Changer l'ordre si nécessaire
     }
+    });
+
+    document.querySelectorAll('.rectangle').forEach((rectangle) => {
+        rectangle.style.display = 'block !important';
     });
 }
 
