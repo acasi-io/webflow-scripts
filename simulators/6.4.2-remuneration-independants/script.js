dd358423e4ca080fdf924d3bcdf0d768533d6620
Lauryne Tranchant <lauryne@macbook-pro-de-lauryne-1.home>
2024-11-19 18:14:10 +0100

    display micro final amount

--- simulators/6.4.2-remuneration-independants/script.js
+++ simulators/6.4.2-remuneration-independants/script.js
@@ -1,5 +1,5 @@
-import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/6.4.1-remuneration-independants/node_modules/publicodes/dist/index.js';
-import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/6.4.1-remuneration-independants/node_modules/modele-social/dist/index.js';
+import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/6.4.2-remuneration-independants/node_modules/publicodes/dist/index.js';
+import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/6.4.2-remuneration-independants/node_modules/modele-social/dist/index.js';
 
 import { calculEurl, storageEurlTotal, fillEurlComparison } from './eurl.js';
 import { microResult, microCalculRetraite, storageMicroTotal, fillMicroComparison } from './micro.js';
@@ -120,11 +120,6 @@ calculBtn.addEventListener('click', () => {
         }
 
         checkUnemploymentAndSocialSecurityProtection();
-
-        if (turnover > 50000) {
-            const microRemuneration = parseInt((document.getElementById('micro-comparison-wage').textContent).replace(/\s+/g, ""));
-            document.getElementById('comparison_fourth_best_remuneration').textContent = microRemuneration;
-        }
 
         simulatorResults.classList.remove('hidden');
         simulatorResults.scrollIntoView({
@@ -381,6 +376,11 @@ function orderBestRemuneration(sasuFinalAmount, eurlFinalAmount, eiFinalAmount, 
         if (targetRectangle) {
             targetRectangle.style.display = 'block'; // S'assurer que le rectangle est visible
             targetRectangle.innerHTML = `<p class="comparison_grid_remuneration_text">${valueFormatted}</p>`; // Mettre à jour le contenu avec le texte formaté
+            if (chiffreAffaires > 50000 && item.socialForm === 'MICRO') {
+                // Mettre à jour le rectangle spécifique pour MICRO
+                const microRemuneration = parseInt(item.value); // Convertir en nombre si nécessaire
+                targetRectangle.innerHTML = `<p class="comparison_grid_remuneration_text">${microRemuneration}€</p>`;
+            }
         } else {
             console.warn(`Target rectangle not found for index ${index}`);
         }
