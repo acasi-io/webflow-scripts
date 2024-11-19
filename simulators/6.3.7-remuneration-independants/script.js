26510a5090941f1193e2b40a62f6a9a3c73c5dc0
Lauryne Tranchant <lauryne@macbook-pro-de-lauryne-1.home>
2024-11-19 17:48:23 +0100

    display micro final amount

--- simulators/6.3.7-remuneration-independants/script.js
+++ simulators/6.3.7-remuneration-independants/script.js
@@ -1,5 +1,5 @@
-import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/6.3.6-remuneration-independants/node_modules/publicodes/dist/index.js';
-import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/6.3.6-remuneration-independants/node_modules/modele-social/dist/index.js';
+import Engine,{ formatValue } from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/6.3.7-remuneration-independants/node_modules/publicodes/dist/index.js';
+import rules from 'https://cdn.jsdelivr.net/gh/acasi-io/webflow-scripts/simulators/6.3.7-remuneration-independants/node_modules/modele-social/dist/index.js';
 
 import { calculEurl, storageEurlTotal, fillEurlComparison } from './eurl.js';
 import { microResult, microCalculRetraite, storageMicroTotal, fillMicroComparison } from './micro.js';
@@ -512,8 +512,9 @@ function updateAndSortDivs(sasuFinalAmount, eurlFinalAmount, eiFinalAmount, micr
 function microConditions(turnover) {
     document.querySelector('.comparison_micro_text').style.display = 'none';
     if (turnover > 50000) {
-        // localStorage.setItem('microTotal', 0);
+        localStorage.setItem('microTotal', 0);
         document.querySelector('.comparison_micro_text').style.display = 'block';
+        document.getElementById('comparison_fourth_best_remuneration').textContent = document.getElementById('micro-comparison-wage').textContent;
         /*document.querySelectorAll('.micro_comparison_wage').forEach((element) => {
             element.textContent = '0€';
         });
