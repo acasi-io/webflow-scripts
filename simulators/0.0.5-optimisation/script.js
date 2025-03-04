6d6d73ef8d6ec591883f79489cdd27a89083d913
Lauryne Tranchant <lauryne@macbook-pro-de-lauryne-1.home>
2025-03-04 17:00:32 +0100

    correct code and add Organisation calcul

--- simulators/0.0.5-optimisation/script.js
+++ simulators/0.0.5-optimisation/script.js
@@ -3,12 +3,71 @@ document.getElementById('start-btn').addEventListener('click', () => {
     document.querySelector('.section_opti-sim').classList.remove('hide');
 });
 
+document.querySelectorAll('.opti-sim_answer-item').forEach((answer) => {
+    answer.addEventListener('mouseover', () => {
+        answer.querySelector('p').style.color = 'white';
+    });
+
+    answer.addEventListener('mouseout', () => {
+        if (!answer.classList.contains('is-selected')) { 
+            answer.querySelector('p').style.color = '#484848';
+        }
+    });
+});
 
 document.addEventListener('DOMContentLoaded', () => {
     let selectedAnswers = {};
 
+    const totalQuestionsByTheme = {
+        administratif: 10,
+        organisation: 6,
+        development: 6
+    };
+
+    const nextButton = document.getElementById('next-btn');
+    const prevButton = document.getElementById('prev-btn');
+    const steps = Array.from(document.querySelectorAll('.opti-sim_question-container'));
+
+    function disableNextButton() {
+        nextButton.classList.add('is-disabled');
+        nextButton.disabled = true;
+    }
+
+    function enableNextButton() {
+        nextButton.classList.remove('is-disabled');
+        nextButton.disabled = false;
+    }
+
+    function updateNextButtonState(questionTheme, questionStep) {
+        const key = `${questionTheme}-${questionStep}`;
+        selectedAnswers[key] ? enableNextButton() : disableNextButton();
+    }
+
+    function applyCurrentThemeOnFirstQuestion(questionTheme) {
+        const currentTheme = document.querySelector(`.opti-sim_theme-item[data-theme="${questionTheme}"]`);
+        if (currentTheme) {
+            currentTheme.classList.add('is-current');
+            currentTheme.querySelectorAll('p').forEach((p) => {
+                p.style.color = '#484848';
+            });
+        }
+    }
+
+    function updateProgressBar(questionTheme) {
+        const totalQuestions = totalQuestionsByTheme[questionTheme];
+        if (!totalQuestions) return;
+    
+        const answeredQuestions = Object.keys(selectedAnswers).filter(key => key.startsWith(questionTheme)).length;
+        const progressPercentage = (answeredQuestions / totalQuestions) * 100;
+    
+        const progressBar = document.querySelector(`.opti-sim_theme-item[data-theme="${questionTheme}"] .opti-sim_progress-bar`);
+        if (progressBar) {
+            progressBar.style.width = `${progressPercentage}%`;
+        }
+    }
+
     function handleAnswerClick(event) {
-        const answerDiv = event.target.closest('[data-answer]');
+        const answerDiv = event.target.closest('.opti-sim_answer-item');
         if (!answerDiv) return;
 
         const questionContainer = answerDiv.closest('.opti-sim_question-container');
@@ -18,18 +77,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
         selectedAnswers[`${questionTheme}-${questionStep}`] = answerValue;
 
-        questionContainer.querySelectorAll('[data-answer]').forEach(div => {
+        questionContainer.querySelectorAll('.opti-sim_answer-item').forEach(div => {
             div.classList.remove('is-selected');
+            div.querySelector('p').style.color = '#484848';
         });
 
         answerDiv.classList.add('is-selected');
+        answerDiv.querySelector('p').style.color = 'white';
 
-        if (questionTheme === 'administratif') {
-            calculAdministratif();
+        if (questionTheme === 'administratif') calculAdministratif();
+        if (questionTheme === 'organisation') calculOrganisation();
+
+        let currentScoreText = document.getElementById(`${questionTheme}-result`);
+        let currentScore = parseInt(currentScoreText.textContent);
+
+        if (currentScore <= 50) {
+            currentScoreText.classList.add('is-bad');
+        } else if (currentScore > 50 && currentScore <= 75) {
+            currentScoreText.classList.add('is-medium');
+        } else {
+            currentScoreText.classList.add('is-good');
         }
+
+        updateProgressBar(questionTheme);
+        enableNextButton();
     }
 
-    document.querySelectorAll('[data-answer]').forEach(answer => {
+    document.querySelectorAll('.opti-sim_answer-item').forEach(answer => {
         answer.addEventListener('click', handleAnswerClick);
     });
 
@@ -84,35 +158,61 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const resultOptimisation = (result / maxResultPossible) * 100;
 
-        console.log(resultOptimisation);
         document.getElementById('administratif-result').textContent = Math.round(resultOptimisation);
     }
 
-    document.getElementById('next-btn').addEventListener('click', () => {
-        const steps = Array.from(document.querySelectorAll('.opti-sim_question-container'));
+    function calculOrganisation() {
+        let numberOfQuestion = 6;
+        let maxResultPossible = numberOfQuestion * 5;
+        let result = 0;
+
+        result = calculThreeAnswers('organisation-11', result);
+        result = calculThreeAnswers('organisation-12', result);
+        result = calculThreeAnswers('organisation-13', result);
+        result = calculThreeAnswers('organisation-14', result);
+        result = calculThreeAnswers('organisation-15', result);
+        result = calculThreeAnswers('organisation-16', result);
+
+        const resultOptimisation = (result / maxResultPossible) * 100;
+
+        document.getElementById('organisation-result').textContent = Math.round(resultOptimisation);
+    }
+
+    function changeQuestion(direction) {
         const activeStep = steps.find(step => !step.classList.contains('hide'));
+        if (!activeStep) return;
+    
         const currentStep = parseInt(activeStep.dataset.step);
-        const nextStep = currentStep + 1;
-
+        const nextStep = direction === 'next' ? currentStep + 1 : currentStep - 1;
         const nextStepElement = steps.find(step => parseInt(step.dataset.step) === nextStep);
+        
+        if (!nextStepElement) return;
+    
+        const questionTheme = nextStepElement.dataset.theme;
+        
+        updateProgressBar(questionTheme);
+    
+        activeStep.classList.add('hide');
+        nextStepElement.classList.remove('hide');
+    
+        applyCurrentThemeOnFirstQuestion(questionTheme);
+        updateNextButtonState(questionTheme, nextStep);
+    
+        prevButton.style.opacity = nextStep === 1 ? 0 : 1;
+    }    
 
-        if (nextStepElement) {
-            activeStep.classList.add('hide');
-            nextStepElement.classList.remove('hide');
-        }
-    });
+    nextButton.addEventListener('click', () => changeQuestion('next'));
+    prevButton.addEventListener('click', () => changeQuestion('prev'));
 
-    document.getElementById('prev-btn').addEventListener('click', () => {
-        const steps = Array.from(document.querySelectorAll('.opti-sim_question-container'));
-        const activeStep = steps.find(step => !step.classList.contains('hide'));
-        const currentStep = parseInt(activeStep.dataset.step);
-        const nextStep = currentStep - 1;
+    function initializeQuiz() {
+        steps.forEach(step => {
+            const questionTheme = step.dataset.theme;
+            const questionStep = step.dataset.step;
+            updateNextButtonState(questionTheme, questionStep);
+        });
 
-        const nextStepElement = steps.find(step => parseInt(step.dataset.step) === nextStep);
+        prevButton.style.opacity = 0;
+    }
 
-        if (nextStepElement) {
-            activeStep.classList.add('hide');
-            nextStepElement.classList.remove('hide');
-        }
-    });
+    initializeQuiz();
 });
